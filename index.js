8e086a9d969fdd29f9beee099686f4d616bee972
NayePort <102191575+NayePort@users.noreply.github.com>
2022-03-24 23:28:15 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+Vue.createApp({
+  data() {
+    return {
+      information: {
+        name: "Cinthia Nayely Portillo Gallardo",
+        id: "A00811827"
+      },
+      hobby: ["bailar", "ver peliculas", "futbol"],
+      Image: "https://as01.epimg.net/meristation/imagenes/2021/12/18/betech/1639817723_888453_1639817851_noticia_normal_recorte1.jpg"
+    };
+  }
+}).mount("#app");
